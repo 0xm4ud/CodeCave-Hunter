6ff0601123fadbc3508dda23457aff8d2b67da09
Felipe P. Donato <56730389+m4udSec@users.noreply.github.com>
2023-01-15 21:45:22 -0300

    Updated cchunter.py
    added automation to retrieve .data section

--- cchunter.py
+++ cchunter.py
@@ -1,44 +1,35 @@
-# (M4UD's CoveCave Hunter)
-
 import pykd
 import sys
 import re
 
-print("\r\nM4UD's CodeCave hunter!\r\n")
+print("\r\nM4UD's Code Cave and .data section hunter!\r\n")
 module_name = ""
 if len(sys.argv) > 1:
     module_name = sys.argv[1]
 
-# step 2: run dd (module name) + 3C L1
 output = pykd.dbgCommand(f"dd {module_name} + 3C L1")
 parts = output.split()
 var = hex(int(parts[1], 16))
 
-# step 4: run dd (module name) + var  + 2c L1
 output = pykd.dbgCommand(f"dd {module_name} + {var} + 2c L1")
 parts = output.split()
 var2 = hex(int(parts[1], 16))
 
-# step 6: run ? (module name) + 1000
 output = pykd.dbgCommand(f"? {module_name} + 1000")
 parts = output.split()
 var3 = parts[4]
 
-# step 8: run !address var3
 output = pykd.dbgCommand(f"!address {var3}")
 
-# step 9: parse the output of !address command
 for line in output.split("\n"):
     if "End Address:" in line:
         var4 = int(line.split()[-1], 16)
     if "Protect:" in line:
         var5 = line.split()[-1]
 
-# step 10: print var4 and var5
 print(f"End Address: 0x{var4:x}")
 print(f"Protect: {var5}")
 
-# step 11: run dd var4 - x
 start_address = None
 end_address = None
 for i in range(0, var4, 4):
@@ -53,10 +44,45 @@ for i in range(0, var4, 4):
         if start_address is not None:
             break
 
-# step 12: print the last dword address filled with 0 and the first dword address filled with 0
 print(f"Code Cave start at: 0x{end_address:x}")
 print(f"Code Cave finishes at: 0x{start_address:x}")
 size = pykd.dbgCommand(f"? 0x{end_address:x} - 0x{start_address:x}")
 match = re.search(r': (.*) =', size)
 value = match.group(1)
-print(f"Size of codecave: {value}")
+print(f"Size of codecave: {value} bytes")
+
+print("\r\n=====================")
+print("Finding .data section...")
+print("=====================\r\n")
+output = pykd.dbgCommand(f"!dh -a {module_name}")
+
+lines = output.split("\n")
+for i in range(len(lines)):
+    if ".data" in lines[i]:
+        var = lines[i+1]
+        var2 = lines[i+2]
+        break
+
+var3 = re.search(r'([\w]+)\s+virtual size',var).group(1)
+var4 = re.search(r'([\w]+)\s+virtual address',var2).group(1)
+print("virtual size = "+var3)
+print("virtual address = "+var4)
+
+output = pykd.dbgCommand(f"? {module_name} + {var3} + {var4}")
+
+var5 = re.search(r'=\s+([\w]+)',output).group(1)
+print("SECTION HEADER .data located at = "+var5)
+
+output = pykd.dbgCommand(f"!vprot {var5}")
+
+protect = re.search(r'(?m)^Protect:\s+(\w+)*',output).group(1)
+if protect == "00000004":
+	print("Protect: "+protect +"  PAGE_READWRITE")
+else:
+	print("NOT PAGE_READWRITE, something is off!")
+    quit()
+
+output = pykd.dbgCommand(f"?{var5} - {module_name}")
+match = re.search(r': (.*) =', output)
+value = match.group(1)
+print("Offset to writable .data section is: "+ hex(int(value)))
